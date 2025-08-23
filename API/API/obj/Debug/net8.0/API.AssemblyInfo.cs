4d2c4e12f62a26afd975993a09230133fe228b08
tfnt128 <thiago.fontoura120@gmail.com>
2025-08-23 11:55:11 -0300

    GitIgnore 2 add

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc71e171208fcb69c8743d9d3a1abc56528b5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0f57165dfabb210c290ceb0d4d2a273af5440b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
